7f66756b946151bf3ce5833193908f0d5ca7a888
Hoson <hoson_zhong@hotmail.com>
2019-03-01 17:03:55 +1100

    Unity Project Completed till Car exercise

---
+++ Advanced Physics/Assets/Scripts/CannonBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody))]
+public class CannonBall : MonoBehaviour
+{
+    public float forceOnFire = 300;
+
+    bool fire = false;
+    bool canFire = true;
+
+    Rigidbody rigidBody = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		if (Input.anyKeyDown && canFire)
+        {
+            rigidBody.isKinematic = false;
+            rigidBody.AddForce(transform.forward * forceOnFire);
+            canFire = false;
+        }
+	}
+}
---
+++ Advanced Physics/Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsObject : MonoBehaviour
+{
+    public Material awakeMat = null;
+    public Material sleepMat = null;
+
+    private Rigidbody _rigidbody = null;
+
+    bool wasSleeping = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    private void FixedUpdate()
+    {
+        if (_rigidbody.IsSleeping() && !wasSleeping && sleepMat != null)
+        {
+            wasSleeping = true;
+            GetComponent<MeshRenderer>().material = sleepMat;
+        }
+
+        if (!_rigidbody.IsSleeping() && wasSleeping && awakeMat != null)
+        {
+            wasSleeping = false;
+            GetComponent<MeshRenderer>().material = awakeMat;
+        }
+    }
+}
