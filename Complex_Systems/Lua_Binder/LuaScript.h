bde6bcc989c5620ecf2039843aa8f76ee4821e22
Hoson <hoson_zhong@hotmail.com>
2019-06-06 16:49:01 +1000

    Redoing Lua Library

---
+++ Complex_Systems/Lua_Binder/LuaScript.h
@@ -0,0 +1,22 @@
+#ifndef LUASCRIPT_H
+#define LUASCRIPT_H
+
+extern "C"
+{
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
+
+class LuaScript
+{
+public:
+
+	LuaScript();
+	~LuaScript();
+
+private:
+
+};
+
+#endif // !LUASCRIPT_H
