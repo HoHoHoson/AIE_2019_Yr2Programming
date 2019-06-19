7851e2822e0a6b35d73a40db4bacfe4c41f9ef6f
Hoson <hoson_zhong@hotmail.com>
2019-06-19 16:58:17 +1000

    Table basic works

--- Complex_Systems/Lua_Binder/LuaScript.cpp
+++ Complex_Systems/Lua_Binder/LuaScript.cpp
@@ -5,20 +5,26 @@ LuaScript::LuaScript(const std::string & filename)
 	// Creates and assigns Lua State
 	m_L = luaL_newstate();
 
-	if (luaL_loadfile(m_L, filename.c_str()) || lua_pcall(m_L, 0, 0, 0))
+	if (luaL_loadfile(m_L, filename.c_str())) // Attempts to load the script at [filepath] onto the Lua stack, it needs to be ran by lua_pcall before it can be used
 	{
-		std::cout << "Error: Lua script not loaded (" << filename << ")." << std::endl;
+		std::cout << "Error: " << lua_tostring(m_L, -1) << std::endl; // Print error message on script loading failure
 
+		lua_close(m_L);
 		m_L = nullptr;
 	}
 	else
-		luaL_openlibs(m_L);
+	{
+		luaL_openlibs(m_L); // Load Lua libraries
+
+		if (lua_pcall(m_L, 0, 0, NULL) != 0) // Calls the script that was placed on the stack. It runs the script and/or makes its variables and functions available for use
+			std::cout << "Error: " << lua_tostring(m_L, -1) << std::endl;
+	}
 }
 
 LuaScript::~LuaScript()
 {
 	// Closes Lua State on destruction
-	if (!isNull())
+	if (m_L != nullptr)
 		lua_close(m_L);
 }
 
@@ -39,13 +45,48 @@ std::vector<std::string> LuaScript::getTableKeys(const std::string & table_name)
 
 	if (luaGetToStack(table_name))
 	{
-		lua_pushnil(m_L);
+		//lua_pushnil(m_L);
 
-		while (lua_next(m_L, -2))
-		{
-			lua_pop(m_L, 1);
-			vec.push_back(lua_tostring(m_L, -1));
-		}
+		//while (lua_next(m_L, -2))
+		//{
+		//	lua_pop(m_L, 1);
+		//	vec.push_back(lua_tostring(m_L, -1));
+		//}
+
+		std::string function_code =
+			"function codefunc(name) "
+
+			"is_global = true;"
+			"table = nil;"
+			"s = \"struct.tab\";"
+
+			"for w in string.gmatch(s, \"%P+\") do "
+
+			"	if (is_global == true) "
+			"	then "
+			"		table = _G[w] print(\"then\") is_global = false "
+			"	else "
+			"		table = table[w] print(\"else\") "
+			"	end;"
+
+			"	print(table.inside) "
+
+			"end;"
+
+			"end";
+
+		if (luaL_loadstring(m_L, function_code.c_str()) != 0)
+			std::cout << lua_tostring(m_L, -1) << std::endl;
+
+		lua_pcall(m_L, 0, 0, NULL);
+
+		std::string s = "12 biubi fiwhf id 69";
+
+		lua_getglobal(m_L, "codefunc");
+		lua_pushstring(m_L, s.c_str());
+		lua_pcall(m_L, 1, 0, 0);
+
+		//std::cout << "codefunc returned: " << lua_tostring(m_L, -1) << std::endl;
 	}
 
 	luaClearStack();
@@ -54,7 +95,7 @@ std::vector<std::string> LuaScript::getTableKeys(const std::string & table_name)
 
 void LuaScript::printError(const std::string & variable_name, const std::string & reason)
 {
-	std::cout << "Error: Can't get (" << variable_name << "), " << reason << "." << std::endl;
+	std::cout << "Error: can't get (" << variable_name << "), " << reason << "." << std::endl;
 }
 
 void LuaScript::luaClearStack()
--- Complex_Systems/Lua_Binder/main.cpp
+++ Complex_Systems/Lua_Binder/main.cpp
@@ -55,29 +55,36 @@ int main()
 {
 	LuaScript lua_script("../scripts/lua_script.lua");
 
-	std::cout << lua_script.get<float>("struct.tab.inside") << std::endl;
-	std::cout << lua_script.get<std::string>("struct.words") << std::endl;
-	std::cout << lua_script.get<int>("struct.words") << std::endl;
+	//std::cout << lua_script.get<float>("struct.tab.inside") << std::endl;
+	//std::cout << lua_script.get<std::string>("struct.words") << std::endl;
+	//std::cout << lua_script.get<int>("struct.words") << std::endl;
 
-	std::vector<std::string> test_vec(lua_script.getVector<std::string>("struct.intvector"));
+	//std::vector<std::string> test_vec(lua_script.getVector<std::string>("struct.intvector"));
 
-	for (std::string i : test_vec)
-	{
-		std::cout << i << std::endl;
-	}
+	//for (std::string i : test_vec)
+	//{
+	//	std::cout << i << std::endl;
+	//}
 
-	callLuaSum(lua_script, 6, 9);
+	//callLuaSum(lua_script, 6, 9);
 
-	for (std::string s : lua_script.getTableKeys("struct"))
-		std::cout << s << std::endl;
+	//for (std::string s : lua_script.getTableKeys("struct"))
+	//	std::cout << s << std::endl;
 
-	lua_register(lua_script.getState(), "write", l_write);
-	lua_register(lua_script.getState(), "multiply", l_multiply);
+	//if (!lua_script.isNull())
+	//{
+	//	lua_register(lua_script.getState(), "write", l_write);
+	//	lua_register(lua_script.getState(), "multiply", l_multiply);
 
-	lua_getglobal(lua_script.getState(), "test");
-	lua_pcall(lua_script.getState(), 0, 0, NULL);
+	//	lua_getglobal(lua_script.getState(), "test");
+	//	lua_pcall(lua_script.getState(), 0, 0, NULL);
 
-	std::cout << "Stack count = " << lua_gettop(lua_script.getState()) << std::endl;
+	//	std::cout << "Stack count = " << lua_gettop(lua_script.getState()) << std::endl;
+	//}
 
+	lua_script.getTableKeys("struct");
+
+	std::cout << "Press ENTER to close window...";
+	getchar();
 	return 0;
 }
